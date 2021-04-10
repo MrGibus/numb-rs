7f65c11dbeab4c97be8d28e3f4b90ed645d50fb2
MrGibus <arcane2.0@gmail.com>
2021-04-10 14:10:57 +0800

    Fraction experiments

--- src/fraction.rs
+++ src/fraction.rs
@@ -1,19 +1,109 @@
-//! Implements a fraction type for integers
-
+//! Implements a fraction type for unsigned integers
 #![allow(dead_code)]
 
-pub struct Fraction<T> {
+use std::ops::{Add, Sub, Mul, Div};
+use crate::numerics::{gcd, Unsigned};
+use std::fmt::{Display, Formatter};
+
+// TODO: Implement Signs for Fraction, casting from i32 values to Fraction<u64> etc.
+
+// PartialEq and Eq need to be manually implemented as 2/4 = 1/2 for instance
+#[derive(Debug, Eq, PartialEq)]
+pub struct Fraction<T: Unsigned> {
     numerator: T,
     denominator: T,
 }
 
-pub const HALF: Fraction<i32> = Fraction{numerator: 1, denominator: 2};
+pub const HALF: Fraction<u64> = Fraction{numerator: 1, denominator: 2};
 
-/// this could just be a method right now idk why don't ask.
+/// shorthand for creating fractions because for some reason it's possible to create a Fraction<i32>
 macro_rules! frac {
     ($num:expr , $den:expr) => {
-        Fraction{numerator: $num, denominator: $den};
+        Fraction::new($num as u64, $den as u64);
     };
+}
+
+impl<T: Unsigned> Fraction<T> {
+    pub fn new(numerator: T, denominator:T) -> Self{
+        Fraction{
+            numerator,
+            denominator,
+        }
+    }
+
+    pub fn reciprocal(self) -> Self{
+        Fraction{
+            numerator: self.denominator,
+            denominator: self.numerator,
+        }
+    }
+
+    pub fn reduce(self) -> Self{
+        let gcd = gcd(self.numerator, self.denominator);
+        Fraction{
+            numerator: self.numerator / gcd,
+            denominator: self.denominator / gcd,
+        }
+    }
+}
+
+
+impl<T: Unsigned> Display for Fraction<T>{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}/{}", self.numerator, self.denominator)
+    }
+}
+
+impl<T: Unsigned> Add for Fraction<T>{
+    type Output = Fraction<T>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let denominator = self.denominator * rhs.denominator;
+        let numerator = self.numerator * rhs.denominator + rhs.numerator * self.denominator;
+
+        Fraction{
+            numerator,
+            denominator,
+        }
+
+    }
+}
+
+impl<T: Unsigned> Sub for Fraction<T> {
+    type Output = Fraction<T>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let denominator = self.denominator * rhs.denominator;
+        let numerator = self.numerator * rhs.denominator - rhs.numerator * self.denominator;
+
+        Fraction{
+            numerator,
+            denominator,
+        }
+    }
+}
+
+impl<T: Unsigned> Mul for Fraction<T> {
+    type Output = Fraction<T>;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        let denominator = self.denominator * rhs.denominator;
+        let numerator = self.numerator * rhs.numerator;
+
+        Fraction{
+            numerator,
+            denominator,
+        }
+    }
+}
+
+impl <T: Unsigned> Div for Fraction<T>{
+    type Output = Fraction<T>;
+
+    #[allow(clippy::suspicious_arithmetic_impl)]
+    fn div(self, rhs: Self) -> Self::Output {
+        self * rhs.reciprocal()
+    }
 }
 
 #[cfg(test)]
@@ -25,5 +115,49 @@ mod tests {
         let half = frac!(1, 2);
         assert_eq!(half.numerator, HALF.numerator);
         assert_eq!(half.denominator, HALF.denominator);
+    }
+
+    #[test]
+    fn display_test(){
+        let a = frac!(132, 203);
+        assert_eq!(format!("{}",a), "132/203".to_string())
+    }
+
+    #[test]
+    fn add_test(){
+        let a = frac!(1, 2);
+        let b =  frac!(1, 4);
+
+        assert_eq!(a + b, frac!(6, 8));
+    }
+
+    #[test]
+    fn sub_test(){
+        let a = frac!(1, 2);
+        let b = frac!(1, 4);
+
+        assert_eq!(a - b, frac!(2, 8));
+    }
+
+    #[test]
+    fn mul_test(){
+        let a = frac!(1, 2);
+        let b = frac!(2, 5);
+
+        assert_eq!(a * b, frac!(2, 10));
+    }
+
+    #[test]
+    fn div_test(){
+        let a =  frac!(1, 2);
+        let b = frac!(1, 4);
+
+        assert_eq!(a / b, frac!(4, 2));
+    }
+
+    #[test]
+    fn reduction_test(){
+        let a = frac!(157684, 511974);
+        assert_eq!(a.reduce(), frac!(158, 513))
     }
 }
--- src/numerics.rs
+++ src/numerics.rs
@@ -1,4 +1,5 @@
-/// Primitive Number traits for Generic Implementations
+//! Primitive Number traits for Generic Implementations
+#![allow(dead_code)]
 
 use std::fmt::{Display, Debug};
 use std::ops::{Div, Add, MulAssign, AddAssign, Mul, Neg, Sub, SubAssign, DivAssign, RemAssign, Rem};
@@ -11,15 +12,28 @@ pub trait Numeric: Display + Debug + Clone + Copy + PartialOrd<Self>
         + Mul<Output=Self> + MulAssign + Div<Output=Self> + DivAssign
         + Rem<Output=Self> + RemAssign + Sum + Sized{
     const ZERO: Self;
+    const ONE: Self;
+    const TWO: Self;
+
+    #[inline]
+    fn is_even(self) -> bool {
+        self % Self::TWO == Self::ZERO
+    }
+
+    #[inline]
+    fn is_odd(self) -> bool {
+        self % Self::TWO == Self::ONE
+    }
 }
 
 impl_multiple!(Numeric => {
     const ZERO: Self = 0;
+    const ONE: Self = 1;
+    const TWO: Self = 2;
 } for i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
 
-impl Numeric for f32 {const ZERO: Self = 0f32;}
-
-impl Numeric for f64 {const ZERO: Self = 0f64;}
+impl Numeric for f32 {const ZERO: Self = 0f32; const ONE: Self = 1f32; const TWO: Self = 2f32;}
+impl Numeric for f64 {const ZERO: Self = 0f64; const ONE: Self = 1f64; const TWO: Self = 2f64;}
 
 
 /// trait used for Primitive number classification
@@ -27,12 +41,15 @@ pub trait Signed: Neg<Output=Self>{}
 
 impl_multiple!(Signed for i8 i16 i32 i64 i128 isize f32 f64);
 
+pub trait Unsigned: Integer{}
+
+impl_multiple!(Unsigned for u8 u16 u32 u64 u128 usize);
+
 
 /// trait used for Primitive number classification
 pub trait Integer: Numeric{}
 
 impl_multiple!(Integer for i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
-
 
 pub trait Float: Numeric + Signed{
     const EPSILON: Self;
@@ -66,4 +83,54 @@ impl Float for f32 {
 
     #[inline]
     fn from_f32(f: f32) -> Self {f}
+}
+
+/// Greatest common denominator between two unsigned integers using steins algorithm
+/// wikipedia implementation, Research Gate has a number of 'improved' algorithms which could
+/// be implented later, but this will do as a placeholder.
+/// Note: Recursive
+// gcd(0, v) = v, because everything divides zero, and v is the largest number that divides v. Similarly, gcd(u, 0) = u.
+// gcd(2u, 2v) = 2·gcd(u, v)
+// gcd(2u, v) = gcd(u, v), if v is odd (2 is not a common divisor). Similarly, gcd(u, 2v) = gcd(u, v) if u is odd.
+// gcd(u, v) = gcd(|u − v|, min(u, v)), if u and v are both odd.
+pub fn gcd<T: Unsigned>(a: T, b: T) -> T {
+    if a == b || b == T::ZERO{
+        a
+    } else if a == T::ZERO{
+        b
+    } else if a.is_even() {
+        if b.is_odd() {
+            gcd(a / T::TWO, b)
+        } else {
+            T::TWO * gcd(a / T::TWO, b / T::TWO)
+        }
+    } else if b.is_even() {
+            gcd(a, b / T::TWO)
+    } else if a > b {
+            gcd((a - b) / T::TWO, b)
+    } else {
+            gcd((b - a) / T::TWO, a)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn gcd_test(){
+        let a: u32 = 21;
+        let b: u32 = 49;
+        assert_eq!(gcd(a, b), 7);
+
+        let a: u32 = 2599;
+        let b: u32 = 791;
+
+        assert_eq!(gcd(a, b), 113);
+
+        let a: u32 = 410876;
+        let b: u32 = 64417;
+
+        assert_eq!(gcd(a, b), 1741);
+    }
 }
